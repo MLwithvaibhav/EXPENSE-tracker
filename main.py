ac02c862e16189c77e3dd8c8229af051be64daf5
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-09-14 18:15:31 +0530

    added first operation

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# hello world 
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+api = Api(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+
+@app.route('/')
+def home():
+    return jsonify(message="Welcome to the Flask application!")
